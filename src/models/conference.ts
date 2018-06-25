31b897465f79658726d652ef07eb772b15a6b3b2
Kerstin Guenther <kerstin.guenther@europace.de>
2018-06-25 14:59:27 +0200

    added back button

--- src/models/conference.ts
+++ src/models/conference.ts
@@ -4,6 +4,7 @@ import {Contact} from "./contact";
 export class Conference {
   id: string;
   title: string;
+  header: string;
   description: string; // HTMlElement?
   directions: Directions;
   contact: Contact;
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,10 @@ import { Component } from '@angular/core';
 import { SpeakersPage } from '../speakers/speakers';
 import { AgendaPage } from '../agenda/agenda';
 import { HomePage } from '../home/home';
+import { NavController } from 'ionic-angular/navigation/nav-controller';
+import { OverviewPage } from '../overview/overview';
+import { GlobalProvider } from '../../providers/global/global';
+import { ConferenceService } from '../../providers/conference/conference-service';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -13,7 +17,17 @@ export class TabsPage {
   agendaPage = AgendaPage;
   speakerPage = SpeakersPage;
 
-  constructor() {
+  title: string;
 
+  constructor(private navCtrl: NavController,
+              private globalProvider: GlobalProvider,
+              private conferenceService: ConferenceService) {
+    this.conferenceService.getConference(this.globalProvider.conferenceId).then((conference) => {
+      this.title = conference.title;
+    });
+  }
+
+  public goToOverview() {
+    this.navCtrl.push(OverviewPage);
   }
 }
