2eaca4d222ee6604711a144e94cbe5efcb3e121e
geckse <geckse@users.noreply.github.com>
2019-02-15 18:44:59 +0100

    added attendee page

---
+++ src/pages/attendees/attendees.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the AttendeesPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-attendees',
+  templateUrl: 'attendees.html',
+})
+export class AttendeesPage {
+
+  attendees: Array<any>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  }
+
+  
+
+
+}
