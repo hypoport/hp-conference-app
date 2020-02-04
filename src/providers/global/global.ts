a687e4b0c9491a129682b616eb47d1ad2bc24571
geckse <geckse@users.noreply.github.com>
2020-02-04 21:56:15 +0100

    hello 1.1.2 / also fixing invisible statusbar issue on iOS darkmode

--- src/providers/global/global.ts
+++ src/providers/global/global.ts
@@ -28,7 +28,7 @@ export class GlobalProvider {
 	  this._apiURL = ENV.apiROOT;
     this._gaKey = ENV.gaKey;
 	  this._qrPayloadSecret = ENV.qrPayloadSecret;
-	  this._appVersion = '1.1.1';
+	  this._appVersion = '1.1.2';
   }
 
   get conferenceId(): string {
